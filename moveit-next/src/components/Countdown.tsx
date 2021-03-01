50693f9a1b4495833d4bc14046e4427a092efc27
monisato <monicasakane@gmail.com>
2021-03-01 13:35:38 -0300

    desenvolvendo contexto

--- moveit-next/src/components/Countdown.tsx
+++ moveit-next/src/components/Countdown.tsx
@@ -1,9 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Countdown.module.css';
 
+let countdownTimeout: NodeJS.Timeout;
+
 export function Countdown() {
-    const [time, setTime] = useState(25 * 60);
-    const [active, setActive] = useState(false);
+    const { startNewChallenge } = useContext(ChallengesContext);
+
+    const [time, setTime] = useState(0.1 * 60);
+    const [isActive, setIsActive] = useState(false);
+    const [hasFinished, setHasFinished] = useState(false);
 
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
@@ -12,18 +18,32 @@ export function Countdown() {
     const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
+    // ao clicar vai colocar como ativo
     function startCountdown() {
-        setActive(true);
+        setIsActive(true);
+    }
+    
+    // pausa no useEffect()
+    // altera o estado ativo ou inativo
+    // reseta o tempo para 25min
+    function  resetCountdown() {
+        clearTimeout(countdownTimeout);
+        setIsActive(false);
+        setTime(0.1 * 60);
     }
 
     // iniciar cronometro quando tiver active=true e quando time foi alterado, 1000 = segundo
     useEffect(() => {
-        if (active && time > 0) {
-            setTimeout(() => {
+        if (isActive && time > 0) {
+            countdownTimeout = setTimeout(() => {
                 setTime(time - 1);
             }, 1000)
+        } else if (isActive && time === 0) {
+            setHasFinished(true);
+            setIsActive(false);
+            startNewChallenge();
         }
-    }, [active, time])
+    }, [isActive, time])
 
     return (
         <div>
@@ -38,14 +58,36 @@ export function Countdown() {
                     <span>{secondRight}</span>
                 </div>
             </div>
-
-            <button 
-                type="button" 
+            
+            { hasFinished ? (
+                <button 
+                disabled
                 className={styles.countdownButton}
-                onClick={startCountdown}
             >
-                Iniciar um ciclo
+                Ciclo encerrado
             </button>
+            ) : (
+                <>
+                { isActive ? (
+                    <button 
+                        type="button" 
+                        className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                        onClick={resetCountdown}
+                    >
+                        Abandonar o ciclo
+                    </button>
+                ) : (
+                    <button 
+                        type="button" 
+                        className={styles.countdownButton}
+                        onClick={startCountdown}
+                    >
+                        Iniciar um ciclo
+                    </button>
+                ) }
+                </>
+            )}
+
         </div>
     );
 }
